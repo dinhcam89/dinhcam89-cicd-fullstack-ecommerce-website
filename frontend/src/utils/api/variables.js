7f82de08c1f4fe1848d93774de1caca564ef3175
dcamm79 <dinhcam1512@gmail.com>
2024-07-03 19:13:32 +0700

    initial commit

---
+++ frontend/src/utils/api/variables.js
@@ -0,0 +1,8 @@
+export const variables = {
+    BASE_URL: "http://34.142.249.93:5214/api/",
+    USER_API: "http://34.142.249.93:5214/api/user",
+    PRODUCT_API: "http://34.142.249.93:5214/api/product",
+    ORDER_API: "http://34.142.249.93:5214/api/order",
+    PRODUCTSIZE_API: "http://34.142.249.93:5214/api/productsize",
+    ORDERITEM_API: "http://34.142.249.93:5214/api/orderitem",
+}
